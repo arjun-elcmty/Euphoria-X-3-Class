613080d82740767b53c1485dae5c84d584c11644
Arjun <nobarjun@gmail.com>
2024-05-24 18:11:17 +0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,13 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCFUpLCTtPv8BTRwOC5NxAhfxygh0cgvZo",
-  authDomain: "kelas-x-3-781e0.firebaseapp.com",
-  projectId: "kelas-x-3-781e0",
-  storageBucket: "kelas-x-3-781e0.appspot.com",
-  messagingSenderId: "840099918326",
-  appId: "1:840099918326:web:e5edbb692042250d07d7d7",
-  measurementId: "G-XHH7QT6SSJ"
+  apiKey: "AIzaSyC5P6i2HHBEuX6a1sOuwJsSZHSl9bbHZxY",
+  authDomain: "web-kelas-b46da.firebaseapp.com",
+  projectId: "web-kelas-b46da",
+  storageBucket: "web-kelas-b46da.appspot.com",
+  messagingSenderId: "613567661812",
+  appId: "1:613567661812:web:fae02f798e42809f63c0a1"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
